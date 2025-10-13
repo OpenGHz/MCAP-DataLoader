48e04c72132779ff5d4b9ae6442a731b95779d76
Haizhou Ge <1352674740@qq.com>
2025-10-13 20:00:46 +0800

    1. add add_metadata, add_attachment and check_cmd_output in McapCLI 2. add McapDeriveMetadataHandler to write and read derive info for mcap file

--- mcap_data_loader/utils/mcap_utils.py
+++ mcap_data_loader/utils/mcap_utils.py
@@ -1,5 +1,14 @@
 from pymcap import PyMCAP
+from pymcap.core import McapCLIOutput
+from mcap.writer import Writer
+from mcap.reader import McapReader, make_reader
+from typing import Optional, List, Dict, Union, Literal, Any
+from pathlib import Path
 import shutil
+import uuid
+import json
+import tempfile
+import logging
 
 
 class McapCLI(PyMCAP):
@@ -11,3 +20,154 @@ class McapCLI(PyMCAP):
             return super().__get_executable()
         else:
             return executable_path
+
+    def add_metadata(self, file_path: str, name: str, metadata: Dict[str, Any]):
+        """Add metadata to the MCAP file."""
+        pair_str = " ".join([f"{k}={json.dumps(v)}" for k, v in metadata.items()])
+        return self.check_cmd_output(
+            self.run_command(f"add metadata {file_path} -n {name} -k {pair_str}")
+        )
+
+    def add_attachment(
+        self,
+        file_path: str,
+        create_time: int,
+        log_time: int,
+        name: str,
+        media_type: str,
+        data: Union[str, bytes],
+    ):
+        """Add attachment to the MCAP file."""
+        use_tmp_file = False
+        if isinstance(data, bytes):
+            use_tmp_file = True
+            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+                tmp_file.write(data)
+                tmp_file.flush()
+                data = tmp_file.name
+        output = self.run_command(
+            f"add attachment {file_path} -n {name} "
+            f"--content-type {media_type} --creation-time {create_time} "
+            f"--log-time {log_time} -f {data}"
+        )
+        if use_tmp_file:
+            tmp_file.close()
+            Path(tmp_file.name).unlink()
+        return self.check_cmd_output(output)
+
+    def check_cmd_output(self, output: McapCLIOutput):
+        """Check the output of an MCAP CLI command."""
+        if not output.success:
+            raise RuntimeError(
+                f"MCAP command failed with error: {output.stderr.strip()}"
+            )
+
+
+DeriveMetadata = Dict[str, Union[Literal["self", "parents"], List[str]]]
+
+
+class McapDeriveMetadataHandler:
+    """Handler for MCAP derivation metadata."""
+
+    def __init__(self):
+        self.writer = None
+        self.reader = None
+
+    def set_writer(self, writer: Writer, replace: bool = False):
+        """Set the MCAP writer."""
+        if self.writer is not None and not replace:
+            raise ValueError("Writer is already set. Use `replace=True` to replace it.")
+        self.writer = writer
+
+    def set_reader(self, reader: McapReader, replace: bool = False):
+        """Set the MCAP reader."""
+        if self.reader is not None and not replace:
+            raise ValueError("Reader is already set. Use `replace=True` to replace it.")
+        self.reader = reader
+
+    def write(
+        self,
+        self_uuid: str = "",
+        parent_derive_info: Optional[Dict[str, Dict[str, Any]]] = None,
+        parent_file_paths: Optional[List[str]] = None,
+    ):
+        """Add derivation metadata to the MCAP file.
+        The derivation means that this file is derived (computed) from other files, which
+        can be used to ensure reproducibility, traceability and consistency.
+        Args:
+            self_uuid (str): UUID of the current file. If empty, a new UUID will be generated.
+            parent_derive_info (Dict[str, Dict]): A dictionary mapping parent file paths to their derivation metadata.
+            parent_file_paths (List[str]): List of parent file paths. If None, use keys of parent_derive_info.
+        Raises:
+            ValueError: If the writer is not set or if the parent file's derive info is
+                        missing or does not match the given info.
+        """
+        self.parent_derive_info = parent_derive_info or {}
+        self.parent_file_paths = parent_file_paths or self.parent_derive_info.keys()
+        parent_uuids = []
+        for parent_file_path in self.parent_file_paths:
+            # Read parent file derive info
+            with open(parent_file_path, "rb") as f:
+                reader = make_reader(f)
+                parent_info = self._read(reader)
+            given_info = self.parent_derive_info.get(parent_file_path, {})
+            write_parent = False
+            if parent_info:
+                if parent_info != given_info:
+                    raise ValueError(
+                        f"Parent file {parent_file_path} derive info does not match the given info:"
+                        f" {parent_info} != {given_info}"
+                    )
+            elif not given_info:
+                raise ValueError(
+                    f"Parent file {parent_file_path} derive info is missing. Please provide it."
+                )
+            else:
+                parent_info = given_info
+                write_parent = True
+
+            parent_uuid = parent_info.get("self", "")
+            if not parent_uuid:
+                raise ValueError(
+                    f"Parent file {parent_file_path} does not have a valid 'self' UUID."
+                )
+            if write_parent:
+                self.get_logger().info(
+                    f"Writing given derive info for parent file {parent_file_path}"
+                )
+                McapCLI().add_metadata(parent_file_path, "derive", parent_info)
+            parent_uuids.append(parent_uuid)
+        self.writer.add_metadata(
+            "derive",
+            {
+                "self": self_uuid or uuid.uuid1().hex,
+                "parents": json.dumps(parent_uuids),
+            },
+        )
+
+    @staticmethod
+    def _read(reader: McapReader) -> DeriveMetadata:
+        """Get derivation metadata from the MCAP file.
+        Returns:
+            dict: A dictionary containing 'self' and 'parents' UUIDs.
+        """
+        derive = {}
+        for meta in reader.iter_metadata():
+            if meta.name == "derive":
+                derive["self"] = meta.metadata["self"]
+                derive["parents"] = json.loads(meta.metadata.get("parents", "[]"))
+        return derive
+
+    def read(self) -> DeriveMetadata:
+        if self.reader is None:
+            raise ValueError("Reader is not initialized.")
+
+    def finish(self):
+        """Finish writing or close reading the MCAP file."""
+        if self.writer:
+            self.writer.finish()
+        # reader does not have a finish method
+
+    @classmethod
+    def get_logger(cls) -> logging.Logger:
+        return logging.getLogger(cls.__name__)
