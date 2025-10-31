4995a7f727926e3614d609bed23462b147b7ea06
Haizhou Ge <1352674740@qq.com>
2025-10-31 15:02:02 +0800

    now depth < 1 means auto and depth=0 means direct return

--- mcap_data_loader/pipelines/dict_tuple.py
+++ mcap_data_loader/pipelines/dict_tuple.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, NonNegativeInt
+from pydantic import BaseModel
 from collections.abc import Generator
 from mcap_data_loader.pipelines.basis import Pipeline, T
-from typing import Tuple, Dict
+from typing import Tuple, Dict, Union
 
 
 Item = Dict[str, T]
@@ -10,12 +10,13 @@ Item = Dict[str, T]
 class DictTupleConfig(BaseModel):
     """Configuration for DictTuple pipeline."""
 
-    depth: NonNegativeInt = 0
+    depth: int = -1
     """Depth of tuple nesting to flatten. 
-    0 means auto-detect by whether the item is a tuple or not,
+    < 0 means auto-detect by whether the item is a tuple or not,
     which is useful for varying depth of tuples or the depth is unknown.
     A positive integer means flattening up to that depth, which requires
-    the depth to be the same for all items and is faster.
+    the depth to be the same for all items and is faster. 0 means the items
+    are already a flattened dictionary.
     """
     separator: str = "/"
     """Separator used when concatenating prefixes."""
@@ -28,7 +29,13 @@ class DictTuple(Pipeline[Tuple[Item]]):
 
     def __init__(self, config: DictTupleConfig) -> None:
         self.config = config
-        self._func = self._process_auto if config.depth == 0 else self._process_depth
+        self._func = (
+            self._process_auto
+            if config.depth < 0
+            else self._process_depth
+            if config.depth > 0
+            else lambda tp, prefix, depth: tp
+        )
         self._last_sep = config.separator if config.separate_key else ""
 
     def __iter__(self) -> Generator[Item]:
@@ -45,7 +52,9 @@ class DictTuple(Pipeline[Tuple[Item]]):
                     self._tuple_dict[f"{prefix}{i}/{k}"] = v
         return self._tuple_dict
 
-    def _process_depth(self, tp: Tuple[Item], prefix: str, depth: int) -> Item:
+    def _process_depth(
+        self, tp: Union[Tuple[Item], Item], prefix: str, depth: int
+    ) -> Item:
         if depth > 1:
             for i, value in enumerate(tp):
                 self._process_depth(
@@ -78,12 +87,13 @@ if __name__ == "__main__":
 
     for index, tuple_dict in enumerate(
         [
+            {"a": 1, "b": 2},
             ({"/a": 1}, {"/b": 2}),
             (({"/a": 1}, {"/b": 2}), ({"/c": 3}, {"/d": 4}, {"/e": 5})),
         ]
     ):
         dict_tuple = DictTuple(
-            DictTupleConfig(depth=index + 1, separator=".", separate_key=False)
+            DictTupleConfig(depth=index, separator=".", separate_key=False)
         )([tuple_dict])
         start = time.perf_counter()
         result = next(iter(dict_tuple))
