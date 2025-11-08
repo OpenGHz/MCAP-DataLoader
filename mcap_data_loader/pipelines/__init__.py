80a175a9893e05fcc77d1381a5ee34aef4dee268
Haizhou Ge <1352674740@qq.com>
2025-11-08 17:17:28 +0800

    remove dict tuple from pipeline and add map in init

--- mcap_data_loader/pipelines/__init__.py
+++ mcap_data_loader/pipelines/__init__.py
@@ -1,7 +1,7 @@
 from mcap_data_loader.pipelines.cache import Cache, CacheConfig
-from mcap_data_loader.pipelines.dict_tuple import DictTuple, DictTupleConfig
 from mcap_data_loader.pipelines.flatten import Flatten, FlattenConfig
 from mcap_data_loader.pipelines.horizon import Horizon, HorizonConfig
+from mcap_data_loader.pipelines.map import Map, MapConfig
 from mcap_data_loader.pipelines.merge import Merge, MergeConfig
 from mcap_data_loader.pipelines.nested_zip import NestedZip, NestedZipConfig
 from mcap_data_loader.pipelines.pairwise import PairWise, PairWiseConfig
