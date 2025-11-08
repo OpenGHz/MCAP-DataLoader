90a759af701e2ff26c90f08772eafa02e6950550
Haizhou Ge <1352674740@qq.com>
2025-11-08 20:40:57 +0800

    remove stack in callers init

--- mcap_data_loader/callers/__init__.py
+++ mcap_data_loader/callers/__init__.py
@@ -1,10 +1,9 @@
 from mcap_data_loader.callers.chain import CallerChain, CallerChainConfig
 from mcap_data_loader.callers.dict_tuple import DictTuple, DictTupleConfig
 from mcap_data_loader.callers.multi import MultiCaller, MultiCallerConfig
-from mcap_data_loader.callers.stack import BatchStacker, BatchStackerConfig
 from mcap_data_loader.callers.policy import (
     PolicyEvaluationCaller,
     PolicyEvaluationCallerConfig,
 )
-# since the some callers (e.g. reduce, array) depend on torch which will severely slows down loading speed
+# since the some callers (e.g. reduce, array, stack, etc.) depend on torch which will severely slows down loading speed
 # so we do not import them here
