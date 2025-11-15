7f0d6ca26ebfc6d906d9931c0f91b391ef2a704e
Haizhou Ge <1352674740@qq.com>
2025-11-15 09:32:40 +0800

    pipeline can only be called once

--- mcap_data_loader/pipelines/basis.py
+++ mcap_data_loader/pipelines/basis.py
@@ -13,13 +13,21 @@ T = TypeVar("T")
 class Pipeline(CallerBasis[T]):
     """Base class for all pipelines. A pipeline can be viewed
     as a special type of caller that can only accept one iterable
-    parameter and return an iterable value.
+    parameter and return an iterable value and can only be called once.
     """
+
+    @final
+    def config_post_init(self):
+        super().config_post_init()
+        self._called = False
 
     @final
     @validate_call
     def __call__(self, iterable: NonIteratorIterable[Any]) -> Self:
         """Initialize the pipeline with the given iterable."""
+        if self._called:
+            raise RuntimeError("A pipeline instance can only be called once.")
+        self._called = True
         self._iterable = iterable
         return self
 
