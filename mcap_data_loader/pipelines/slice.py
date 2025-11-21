a47ba3b125ec32605174678095c7951b85b00881
Haizhou Ge <1352674740@qq.com>
2025-11-21 11:58:20 +0800

    improve slice

--- mcap_data_loader/pipelines/slice.py
+++ mcap_data_loader/pipelines/slice.py
@@ -1,11 +1,12 @@
 """Slices the data from the start index to the end index with a specified step."""
 
-from collections.abc import Iterator
+from collections.abc import Iterable
 from typing import Optional
 from more_itertools import islice_extended
 from pydantic import BaseModel, NonNegativeInt, PositiveInt
 from mcap_data_loader.pipelines.basis import Pipe, T
 from mcap_data_loader.utils.basic import try_to_get_attr
+from mcap_data_loader.utils.extra_itertools import Reusablizer
 
 
 class SliceConfig(BaseModel, frozen=True):
@@ -21,24 +22,28 @@ class SliceConfig(BaseModel, frozen=True):
     """Stride of the slice. If ``None``, it will be inferred from the input iterable (future_span)."""
 
 
-class Slice(Pipe[T]):
+class Slice(Pipe):
     """Yield items from the iterable according to the configured slice."""
 
     def __init__(self, config: SliceConfig) -> None:
         self.config = config
+        self._islice = Reusablizer(islice_extended)
+        self._ss = (
+            config.start == 0
+            and config.stop is None
+            and (config.step == 1 or config.step is None)
+        )
 
-    def on_call(self, iterable):
+    def on_call(self, iterable: Iterable[T]) -> Iterable[T]:
+        sss = self._ss
         if self.config.step is None:
             step = try_to_get_attr(iterable, ["config.future_span", "future_span"])
+            sss *= step == 1
         else:
             step = self.config.step
-        self._step = step
-        return super().on_call(iterable)
-
-    def __iter__(self) -> Iterator[T]:
-        return islice_extended(
-            self._iterable, self.config.start, self.config.stop, self._step
-        )
+        if sss:  # No slicing needed
+            return iterable
+        return self._islice(iterable, self.config.start, self.config.stop, step)
 
 
 __all__ = ["Slice", "SliceConfig"]
