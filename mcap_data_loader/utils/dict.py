85fdadc7980b91bc579bf66e9e7a666bd7bcf8f9
Haizhou Ge <1352674740@qq.com>
2025-12-20 16:36:26 +0800

    add more dict utils for dict values merging

--- mcap_data_loader/utils/dict.py
+++ mcap_data_loader/utils/dict.py
@@ -1,8 +1,9 @@
-from typing import Tuple, TypeVar, Callable, Any, Dict, Generic
+from typing import Tuple, TypeVar, Any, Dict, Generic, List, Optional, Union
 from typing_extensions import Annotated
-from collections.abc import Mapping, Iterable, Hashable
+from collections.abc import Mapping, Iterable, Hashable, Callable
 from pydantic import BeforeValidator, PlainSerializer
 from cachetools import cached
+from collections import defaultdict
 
 
 T = TypeVar("T")
@@ -108,14 +109,169 @@ def _validate_mapping_call(v: Any):
     return v if hasattr(v, "cache") else cached(cache={})(v)
 
 
-def _serialize_mapping_call(v: Any) -> Dict[K, K]:
+def _serialize_by_cache(v: Any) -> Dict[K, K]:
     return v.cache
 
 
+cache_json_serializer = PlainSerializer(_serialize_by_cache, when_used="json")
+
 MappingCall = Annotated[
-    Callable[[K], K],
-    BeforeValidator(_validate_mapping_call),
-    PlainSerializer(_serialize_mapping_call, when_used="json"),
+    Callable[[K], K], BeforeValidator(_validate_mapping_call), cache_json_serializer
+]
+
+PredCallType = Callable[[Hashable], Tuple[Hashable, int]]
+PredDictType = Dict[Hashable, Tuple[Hashable, int]]
+MergeConfigType = Dict[Hashable, List[Hashable]]
+
+
+def sort_by_priority(values: Iterable, priorities: Iterable) -> list:
+    """
+    Sorts the values based on their corresponding priorities in ascending order
+    (i.e., lower priority numbers indicate higher precedence).
+    Elements with equal priorities retain their original relative order (stable sort).
+
+    Args:
+        values (Iterable): An iterable of values to be sorted.
+        priorities (Iterable): An iterable of priority values corresponding to `values`.
+                               Must have the same length as `values`.
+
+    Returns:
+        list: A list of values sorted by priority in ascending order,
+              preserving original order for items with equal priority.
+
+    Raises:
+        ValueError: If `values` and `priorities` have different lengths.
+    """
+    if len(values) != len(priorities):
+        raise ValueError("values 和 priorities 长度必须相同")
+
+    paired = zip(priorities, values)
+    sorted_pairs = sorted(paired, key=lambda x: x[0])
+    return [value for priority, value in sorted_pairs]
+
+
+def create_merge_config(
+    keys: Iterable[Hashable], pred: PredCallType
+) -> MergeConfigType:
+    ori_key_dict = defaultdict(list)
+    priority_dict = defaultdict(list)
+    for k in keys:
+        new_k, prior = pred(k)
+        ori_key_dict[new_k].append(k)
+        priority_dict[new_k].append(prior)
+    config = {}
+    for new_key, ori_keys in ori_key_dict.items():
+        priorities = priority_dict[new_key]
+        sorted_ori_keys = sort_by_priority(ori_keys, priorities)
+        config[new_key] = sorted_ori_keys
+    return config
+
+
+def merge_values_with_config(
+    d: dict, config: MergeConfigType, method: Callable[[Iterable], Any], *args, **kwargs
+) -> dict:
+    """Merge values in a dictionary based on a merge configuration.
+
+    Args:
+        d: The input dictionary with hashable keys.
+        config: A merge configuration dictionary mapping new keys to lists of original keys.
+        method: A function to merge the list of values for each new_key. Default is sum.
+    Returns:
+        A new dictionary with merged values.
+    """
+    return {
+        new_key: (
+            method((d[k] for k in ori_keys), *args, **kwargs)
+            if len(ori_keys) > 1
+            else d[ori_keys[0]]
+        )
+        for new_key, ori_keys in config.items()
+    }
+
+
+def merge_values_with_pred(
+    d: dict, pred: PredCallType, method: Callable[[Iterable], Any], *args, **kwargs
+) -> dict:
+    """Merge values in a dictionary based on a predicate function.
+
+    Args:
+        d: The input dictionary with hashable keys.
+        pred: A predicate function that takes a key and returns a tuple of (new_key, priority).
+            The values with the same new_key will be merged based on their priority, i.e.
+            lower priority values will be considered first.
+        method: A function to merge the list of values for each new_key. Default is sum.
+    Returns:
+        A new dictionary with merged values.
+    """
+    value_dict = defaultdict(list)
+    priority_dict = defaultdict(list)
+    for k, v in d.items():
+        new_k, prior = pred(k)
+        value_dict[new_k].append(v)
+        priority_dict[new_k].append(prior)
+    merged_dict = {}
+    for key, values in value_dict.items():
+        priorities = priority_dict[key]
+        if len(priorities) == 1:
+            merged_dict[key] = values[0]
+        else:
+            sorted_values = sort_by_priority(values, priorities)
+            merged_dict[key] = method(sorted_values, *args, **kwargs)
+    return merged_dict
+
+
+class MergeValuesCall:
+    """A callable class to merge dictionary values based on a predicate or configuration."""
+
+    def __init__(
+        self,
+        method: Callable[[Iterable], Any],
+        pred: Optional[Union[PredCallType, PredDictType]] = None,
+        config: Optional[MergeConfigType] = None,
+        *args,
+        **kwargs,
+    ):
+        # TODO: use a single config class as input
+        if not callable(pred):
+            pred = CallableKeyMappingDict(pred)
+        self.method = method
+        self.pred = pred
+        self.config = config
+        self.args = args
+        self.kwargs = kwargs
+        if None not in (pred, config):
+            raise ValueError("Only one of pred or config can be provided.")
+        elif (pred, config) == (None, None):
+            raise ValueError("One of pred or config must be provided.")
+
+    def __call__(self, d: dict) -> dict:
+        if self.config is None:
+            self.config = create_merge_config(d.keys(), self.pred)
+        return merge_values_with_config(
+            d, self.config, self.method, *self.args, **self.kwargs
+        )
+
+    @property
+    def cache(self) -> Dict:
+        return self.config
+
+
+def pass_through(data):
+    return data
+
+
+def _validate_merge_values_call(v):
+    if not callable(v):
+        if v:
+            return MergeValuesCall(**v)
+        return pass_through
+    return v
+
+
+MergeValuesCallType = Annotated[
+    Callable[[dict], dict],
+    BeforeValidator(_validate_merge_values_call),
+    cache_json_serializer,
 ]
 
 
@@ -153,15 +309,56 @@ if __name__ == "__main__":
     # result_depth_2 = valmap_depth(lambda x: x + 10, simple_dict, depth=1)
     # print(result_depth_2)
 
+    # from pprint import pprint
+    # from pydantic import BaseModel
+
+    # class TestMappingDictModel(BaseModel):
+    #     mapping: MappingCall[str]
+
+    # test_dict = {"a": "alpha", "b": "beta"}
+    # model_instance = TestMappingDictModel(mapping=test_dict)
+    # pprint(model_instance.model_dump(mode="json"))
+    # model_instance = TestMappingDictModel(mapping=lambda x: x.upper())
+    # model_instance.mapping("gamma")
+    # pprint(model_instance.model_dump(mode="json"))
+
+    data = {
+        "b": [20],
+        "a": [10],
+        "d": [40],
+        "c": [30],
+        "e": 0,
+    }
+
+    def predicate(key):
+        if key in ["a", "b"]:
+            return ("group1", key)  # group1
+        elif key in ["c", "d"]:
+            return ("group2", 0)  # group2
+        else:
+            return (key, 0)  # keep original key
+
+    merged = merge_values_with_pred(data, predicate, method=sum, start=[])
+
+    def assert_merged(merged):
+        assert merged["group1"] == [10, 20]
+        assert merged["group2"] == [40, 30]
+        assert merged["e"] == 0
+
+    assert_merged(merged)
+
+    config = create_merge_config(data.keys(), predicate)
     from pprint import pprint
-    from pydantic import BaseModel
 
-    class TestMappingDictModel(BaseModel):
-        mapping: MappingCall[str]
+    print("Merge Config:")
+    pprint(config)
 
-    test_dict = {"a": "alpha", "b": "beta"}
-    model_instance = TestMappingDictModel(mapping=test_dict)
-    pprint(model_instance.model_dump(mode="json"))
-    model_instance = TestMappingDictModel(mapping=lambda x: x.upper())
-    model_instance.mapping("gamma")
-    pprint(model_instance.model_dump(mode="json"))
+    merged_with_config = merge_values_with_config(data, config, method=sum, start=[])
+    assert_merged(merged_with_config)
+
+    print("Merged Result:")
+    pprint(merged_with_config)
+
+    merge_call = MergeValuesCall(method=sum, pred=predicate, start=[])
+    merged_with_call = merge_call(data)
+    assert_merged(merged_with_call)
