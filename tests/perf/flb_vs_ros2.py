109c67dbb115d4900558a03e359ed02bb974ca70
Haizhou Ge <1352674740@qq.com>
2026-03-21 20:47:38 +0800

    A new performance testing script, flb_vs_ros2.py, has been added to compare the read times of McapROSReader and McapFlatBuffersReader.

---
+++ tests/perf/flb_vs_ros2.py
@@ -0,0 +1,37 @@
+from mcap_data_loader.serialization.ros import McapROSReader
+from mcap_data_loader.serialization.flb import McapFlatBuffersReader
+from pathlib import Path
+from time import perf_counter
+import numpy as np
+
+
+path = Path("data/ros/0.mcap")
+assert path.exists(), f"Data file does not exist: {path}"
+
+with open(path, "rb") as f:
+    reader = McapROSReader(f)
+
+    times = []
+    start = perf_counter()
+    key = "/robot/arm_right_lead/poses"
+    for i, sample in enumerate(reader.iter_message_samples([key])):
+        # print(sample)
+        pos = sample[key]["data"].pose.position
+        position = np.array([pos.x, pos.y, pos.z])
+    end = perf_counter()
+    print(f"Time taken to read {i+1} samples: {end - start:.4f} seconds")
+
+path = Path("data/flb/0.mcap")
+assert path.exists(), f"Data file does not exist: {path}"
+
+with open(path, "rb") as f:
+    reader = McapFlatBuffersReader(f)
+
+    times = []
+    start = perf_counter()
+    key = "/right/lead/eef/pose/position"
+    for i, sample in enumerate(reader.iter_message_samples([key])):
+        position = sample[key]["data"]
+        # print(sample)
+    end = perf_counter()
+    print(f"Time taken to read {i+1} samples: {end - start:.4f} seconds")
